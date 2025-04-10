65c045fb54266a84921eafc9bb717db607f50fde
MD Sulaiman <thcgame2580@gmail.com>
2025-04-11 01:33:47 +0600

    Delete client directory

--- client/callControl.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".history").onclick = function () {
-  if (document.querySelector(".historyPanel").style.display === "none") {
-    document.querySelector(".historyPanel").style.display = "block"
-  }
-
-  else {
-    document.querySelector(".historyPanel").style.display = "none"
-  }
-};
